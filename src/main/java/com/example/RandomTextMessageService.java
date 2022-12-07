5155baea748b6d9a9b9d6b0192aef60354f0587a
JayCBoi <zbinskip@gmail.com>
2022-12-07 18:50:25 +0100

    Ex. 1-15

---
+++ src/main/java/com/example/RandomTextMessageService.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.stereotype.Component;
+
+@Component("com.example")
+
+public class RandomTextMessageService implements MessageService{
+
+    private final String[] message = {"Witaj [0]", "Witaj [1]","Witaj [2]","Witaj [3]","Witaj [4]","Witaj [5]","Witaj [6]","Witaj [7]","Witaj [8]","Witaj [9]",};
+
+    @Override
+    public String getMessage() {
+        return message[(int) (Math.random() * (9))];
+    }
+}
